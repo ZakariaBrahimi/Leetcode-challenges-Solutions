3122cde8aafa7673ed3b245b47831791b0dcb3ae
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-05-05 11:25:26 +0100

    Time: 74 ms (27.89%), Space: 14.7 MB (36.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if sorted(t) == sorted(s):
+                return True
+        return False
