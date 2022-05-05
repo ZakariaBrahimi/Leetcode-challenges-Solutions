867865a01b608a86100a099580f9d359d7e76d50
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-05-05 12:13:32 +0100

    Time: 41 ms (82.23%), Space: 13.7 MB (97.40%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -7,6 +7,8 @@ class Solution(object):
         """
         t_hashMap = {}
         s_hashMap = {}
+        if len(s) != len(t):
+            return False
         for letter in s:
                 if letter in s_hashMap:
                         s_hashMap[letter] += 1
@@ -18,6 +20,9 @@ class Solution(object):
                         t_hashMap[letter] += 1
                 else:
                         t_hashMap[letter] = 1
+                        
+        
+            
         
         if s_hashMap == t_hashMap:
                 return True
