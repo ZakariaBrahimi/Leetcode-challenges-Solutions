ee4f9a7bf4d46e83c5daa941c1289bd04947dff2
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-05-05 09:34:59 +0100

    Time: 397 ms (77.58%), Space: 24 MB (42.99%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashMap = {}
+        for num in nums:
+                if num in hashMap:
+                        return True
+                else:
+                        hashMap[num] = 1
+        return False
+        
