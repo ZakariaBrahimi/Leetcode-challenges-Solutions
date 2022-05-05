70e9afe77252d2ca09f327f2c4c34b7c7fe85a77
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-05-05 18:51:12 +0100

    Time: 147 ms (5.95%), Space: 42.5 MB (96.61%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    if(numbers.length == 2){
+        return [1, 2]
+    }
+    leftPointer = 0 
+    rightPointer = numbers.length - 1
+    while (leftPointer < rightPointer){
+        sum = numbers[leftPointer] + numbers[rightPointer]
+        if (sum == target){
+                return [leftPointer+1, rightPointer+1]
+        }else{
+            if(sum < target){
+                leftPointer += 1
+            }else{
+                rightPointer -= 1
+            }
+            
+        }
+    }
+};
