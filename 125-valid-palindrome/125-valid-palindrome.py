ba040c201bb2cf663f65e517612a43d0a2fe4fda
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-05-06 09:42:45 +0100

    Time: 2586 ms (5.00%), Space: 14.3 MB (65.26%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,8 +1,12 @@
 class Solution(object):
     def toValidString(self, s):
+        # "A man, a plan, a canal: Panama"
         for char in s:
-                if not char.isalnum():
-                        s = s.replace(char, '') 
+                if not (ord('A') <= ord(char) <= ord('Z') or
+                    ord('a') <= ord(char) <= ord('z') or
+                    ord('0') <= ord(char) <= ord('9')
+                    ):
+                        s = s.replace(char, '')  
         return s.lower()
 
     def isPalindrome(self, s):
