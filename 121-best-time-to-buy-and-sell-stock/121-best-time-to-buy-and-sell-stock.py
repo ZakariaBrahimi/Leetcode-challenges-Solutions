1b7c08b6c892f55880f928bb86a2b270a3303209
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-05-06 15:17:20 +0100

    Time: 1005 ms (53.19%), Space: 22.8 MB (30.12%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) == 1: return 0
+        left  = 0 # buy
+        right = 1 # sell
+        profit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                currentProfit = prices[right] - prices[left] # = 4 - 1 = 3
+                if currentProfit > profit:
+                    profit = currentProfit
+            else:
+                left = right
+            right += 1
+        return profit
+        
