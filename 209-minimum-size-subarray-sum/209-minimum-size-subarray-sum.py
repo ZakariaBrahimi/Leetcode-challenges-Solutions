7e2d709cb9b3531a002c517867fc1495a21faacf
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-06 19:02:19 +0100

    Time: 472 ms (8.16%), Space: 23.8 MB (6.34%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,21 +1,14 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
-        """
-        
-        target = 7
-        nums = [2,3,1,2,4,3]
-                          l
-                          r
-        """
+
         left = 0
-        result = sys.maxsize # 2
-        current_sum = 0 # 2+3+1+2=8-2=6+4=10-3=7-1=6+3=9-2=7-4=3
-        result_value_has_changed = False
+        result = sys.maxsize
+        current_sum = 0 
+        
         for right in range(len(nums)):
             current_sum += nums[right]
             while current_sum >= target:
                 result = min(result, right-left+1)
-                result_value_has_changed = True
                 current_sum -= nums[left]
                 left += 1
         
