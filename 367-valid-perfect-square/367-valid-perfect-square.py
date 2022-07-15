8924cd527c765255f52ca02363a42922c506174b
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-15 21:14:05 +0100

    Time: 15 ms (91.72%), Space: 13.4 MB (61.96%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        1,4,9, 16  25  36  49  64  81  100  110
+        1,2,3, 4,  5,  6,  7,  8,  9,  10,  11,  12,13,14,16
+        l      m               r                          
+        
+        """
+        left = 1
+        right = num
+        
+        while left <= right:
+            middle = (left + right) // 2
+            if middle*middle == num:
+                return True
+            elif middle*middle < num:
+                left = middle + 1
+            else:
+                right = middle - 1
+        
+
+        
+        
