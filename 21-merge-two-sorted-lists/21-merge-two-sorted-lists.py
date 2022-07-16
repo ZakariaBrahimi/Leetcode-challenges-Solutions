f77cda213ea1661393ad062c4e039a655824bc83
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-16 15:31:38 +0100

    Time: 22 ms (93.40%), Space: 13.6 MB (31.73%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,10 +5,9 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        result_list = current = ListNode()
+        result_list = current = ListNode() # 0 -> None
         i = list1
         j = list2
-        #current = result_list
         
         while (i != None and j != None):            
             if i.val < j.val:
@@ -30,4 +29,4 @@ class Solution(object):
             current = current.next
             i = i.next
             
-        return result_list.next
+        return result_list.next # The actual first node value in the result list is 0 because of the first initialization ===>> 0->1->2->...->k
