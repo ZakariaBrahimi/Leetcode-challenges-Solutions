c62b7ef820fe7657d88aa59896dc9ec2f322ca98
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-24 20:39:22 +0100

    Time: 28 ms (41.27%), Space: 13.3 MB (93.05%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -11,7 +11,7 @@ class Solution(object):
         
         # Recursive Approach
         
-        result = list()
+        """result = list()
         # Base Case
         if not root:
             return []
@@ -19,5 +19,54 @@ class Solution(object):
         result += self.postorderTraversal(root.left)
         result += self.postorderTraversal(root.right)
         result.append(root.val)
+        return result"""
+    
+        # Iterative Approach
+        # POST-Order Traversing: left -> right -> Node
+        # Keep go to the left until reach the limit of the tree + at the same time we visited the node add it to the stack
+        # Than check if this last node have a right node or not !
+        # if yes, go for it, than go left again and again
+        # otherwise, append it to the result, and pop it from the stack
+        result = []
+        stack = []
+        current = root
+        
+        while(current or stack):
+            while current != None:
+                stack.append(current)
+                current = current.left
+            temp = stack[-1]
+            if temp.right != None:
+                current = temp.right
+            else:
+                result.append(temp.val)
+                temp = stack.pop()
+                while stack and temp == stack[-1].right:
+                    temp = stack.pop()
+                    result.append(temp.val)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """while(current or stack):
+            while current != None:
+                stack.append(current)
+                current = current.left
+            current = stack[-1]
+            if current.right == None:
+                current = stack.pop()
+                result.append(current.val)
+            else:
+                current = current.right
+            """
         return result
+                
+        
+        
         
