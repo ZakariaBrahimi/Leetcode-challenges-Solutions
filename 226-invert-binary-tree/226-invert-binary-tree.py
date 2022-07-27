29fb947978e0f9f778a17d9e60109bd8d97d827a
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-27 16:35:45 +0100

    Time: 33 ms (26.02%), Space: 13.5 MB (20.68%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -7,8 +7,7 @@
 class Solution(object):
     def invertTree(self, root):
         if not root: return 
-        if not root.left and not root.right:
-            return root
+        
         
         temp = root.left
         root.left = root.right
@@ -18,30 +17,4 @@ class Solution(object):
         
         return root
         
-        """stack = []
-        current = root
         
-        if not root: return None
-        
-        k = root.left
-        root.left = root.right
-        root.right = k
-        
-        while stack or current:
-            while current:
-                stack.append(current)
-                current = current.left
-            temp = stack[-1]
-            if temp.right:
-                current = temp.right
-            else:
-                stack.pop()
-                temp = stack[-1]
-                ch = temp.left
-                temp.left = temp.right
-                temp.right = ch
-                temp = temp.right
-                while stack and stack[-1].right == temp:
-                    temp = stack.pop()
-        
-        return root"""
