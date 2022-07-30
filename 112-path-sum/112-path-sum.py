b742206f2eba21780b5c93160af01ab08d8b6f37
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-30 10:54:47 +0100

    Time: 38 ms (74.03%), Space: 15.5 MB (22.86%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -5,7 +5,51 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def hasPathSum(self, node, s):
+    def hasPathSum(self, root, targetSum):
+        """if not root:
+            return False
+        
+        current = root
+        result = 0
+        stack = [1,]
+        
+        while stack or current:
+            while current:
+                result += current.val
+                stack.append(current)
+                current = current.left
+            temp = stack[-1]
+            if temp.right:
+                current = temp.right
+            else:
+                if (not temp.left) and (not temp.right) and (result == targetSum):
+                    return True
+                temp = stack.pop()
+                result -= temp.val
+                while temp or stack or stack[-1].right == temp:
+                    temp = stack.pop()
+                    result -= temp.val
+        
+        return False"""
+        
+        
+        
+        current_sum = 0
+        def dfs(root, current_sum):
+            current = root
+            if not current: 
+                return False
+            current_sum += current.val
+            if not current.left and not current.right:
+                return targetSum == current_sum
+            
+            return (dfs(current.left, current_sum) or
+            dfs(current.right, current_sum)
+            )
+        return dfs(root, current_sum)
+        
+        """
+        def hasPathSum(self, node, s):
         if not node: return False
         ans = 0
         subSum = s - node.val
@@ -22,6 +66,7 @@ class Solution(object):
             ans = ans or self.hasPathSum(node.right, subSum)
  
         return ans
+        """
         
         
         
