db3b56cf569513751119e8f517305a4962f1dbc8
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-07-31 11:49:11 +0100

    Time: 1611 ms (5.01%), Space: 95 MB (36.79%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -6,15 +6,14 @@
 #         self.right = right
 class Solution(object):
     def minDepth(self, root):
-        left = 0
-        right = 0
-        def dfs(root, left, right):
+        if not root: return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        return 1 + min(left, right) if (left and right) else 1 + max(left, right)
+        
+        """def dfs(root, left, right):
             if not root: return 0
             left = dfs(root.left, left, right)
             right = dfs(root.right, left, right)
-            """if left == 0 and right!= 0:
-                return 1 + max(left, right)
-            if right == 0 and left!= 0:
-                return 1 + max(left, right)"""
             return 1 + min(left, right) if (left and right) else 1 + max(left, right)
-        return dfs(root, left, right)
+        return dfs(root, left, right)"""
