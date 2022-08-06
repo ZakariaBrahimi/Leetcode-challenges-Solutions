13e394816e44074d3f80a56bd4d12f9a51bf6b88
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-06 10:20:32 +0100

    Time: 14291 ms (62.04%), Space: 119.4 MB (40.46%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.word = True
+
+    def search(self, word):
+        return self.searchFrom(self.root, word)
+
+    def searchFrom(self, node, word):
+        for i in xrange(len(word)):
+            c = word[i]
+            if c == '.':
+                for k in node.children:
+                    if self.searchFrom(node.children[k], word[i+1:]):
+                        return True
+                return False
+            elif c not in node.children:
+                return False
+            node = node.children[c]
+        return node.word
