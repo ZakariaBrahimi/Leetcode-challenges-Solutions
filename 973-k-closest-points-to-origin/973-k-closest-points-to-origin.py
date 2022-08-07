a47fde741fd6ded86e265e6593c2ce858eed1694
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-07 15:15:58 +0100

    Time: 1166 ms (32.44%), Space: 19.7 MB (68.40%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        min_heap = [[math.sqrt(math.pow(point[0], 2) + math.pow(point[1], 2)), point] for point in points]  
+        heapq.heapify(min_heap)
+        result = list()
+        while k != 0:
+            closest = heapq.heappop(min_heap)
+            result.append(closest)
+            k -= 1
+        return [point[1] for point in result]
