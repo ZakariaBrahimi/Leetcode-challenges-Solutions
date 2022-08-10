6f54095f3fb50573717c922f41a2781416e0c371
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-10 18:16:18 +0100

    Time: 3325 ms (5.02%), Space: 31.8 MB (5.01%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,8 +1,37 @@
 class Solution(object):
+    
     def findKthLargest(self, nums, k):
-        min_heap = [-num for num in nums]
+        # Quick Select Approach
+        k = len(nums) - k
+        def quickSelect(left, right):
+            pivot = nums[right]
+            pointer = left
+            
+            for i in range(left, right):
+                if pivot >= nums[i]:
+                    nums[pointer], nums[i] = nums[i], nums[pointer]
+                    pointer += 1
+            
+            nums[pointer], nums[right] = pivot, nums[pointer]
+            if pointer == k: return nums[pointer]
+            elif pointer < k: return quickSelect(pointer+1, right)
+            else: return quickSelect(left, pointer-1)
+            
+            
+            
+        return quickSelect(0, len(nums) - 1)
+    
+    
+    
+    # This is working solution using Heap Data Structure
+    # Time Complexity: O(n), where n is the length of array
+    # Space Complexity: O(1), No extra memory !
+    """
+    def findKthLargest(self, nums, k):
+        min_heap = [-num for num in nums] # it's simulation to Max Heap
         heapq.heapify(min_heap)
         while k != 0:
             output = heapq.heappop(min_heap)
             k -= 1
         return -output
+    """
