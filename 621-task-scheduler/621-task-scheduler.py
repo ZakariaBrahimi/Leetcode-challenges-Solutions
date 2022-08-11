5aa966c3aa6c8e8874b6e0223123d05647690006
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-11 16:29:43 +0100

    Time: 620 ms (57.00%), Space: 15.5 MB (70.14%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def leastInterval(self, tasks, n):
+        # Solving the problem by figuring out the pattern
         count = collections.Counter(tasks) # {letter:occurence}
         freq = [val for val in count.values()]
         max_freq = max(freq)
@@ -8,27 +9,27 @@ class Solution(object):
         return max(len(tasks), (max_freq - 1) * (n + 1) +  max_freq_tasks)
         
         
-        
+        # Max Heap && Queue Approach
         """
         def leastInterval(self, tasks, n):
-        count = collections.Counter(tasks) # {letter:occurence}
-        max_heap = [-val for val in count.values()]
-        heapq.heapify(max_heap) # [], n = 2
-        time_required = 0 # 12
-        queue = [] # [ [1,12]  ]
+            time_required = 0
+            count         = collections.Counter(tasks) # {letter:occurence}
+            queue         = []
+            max_heap      = [-val for val in count.values()]
+            heapq.heapify(max_heap) 
         
-        while max_heap or queue:
-            time_required += 1
-            if max_heap:
-                current_task  = 1 + heapq.heappop(max_heap) # 1
-                if current_task:
-                    rest = time_required + n # 12
-                    queue.insert(0, [current_task, rest])
-            if queue and queue[-1][1] == time_required:
-                val = queue.pop()[0] # 0
-                heapq.heappush(max_heap, val)
+            while max_heap or queue:
+                time_required += 1
+                if max_heap:
+                    current_task  = 1 + heapq.heappop(max_heap)
+                    if current_task:
+                        rest = time_required + n # 12
+                        queue.insert(0, [current_task, rest])
+                if queue and queue[-1][1] == time_required:
+                    val = queue.pop()[0] # 0
+                    heapq.heappush(max_heap, val)
             
-        return time_required
+            return time_required
         """
             
         
