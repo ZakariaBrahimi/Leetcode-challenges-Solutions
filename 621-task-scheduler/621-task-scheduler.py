68e6a7d0f30fc0c357488d82709c42aee86f062a
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-11 12:48:31 +0100

    Time: 1176 ms (9.06%), Space: 15.7 MB (50.85%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        count = collections.Counter(tasks) # {letter:occurence}
+        max_heap = [-val for val in count.values()]
+        heapq.heapify(max_heap) # [], n = 2
+        time_required = 0 # 12
+        queue = [] # [ [1,12]  ]
+        
+        while max_heap or queue:
+            time_required += 1
+            if max_heap:
+                current_task  = 1 + heapq.heappop(max_heap) # 1
+                if current_task:
+                    rest = time_required + n # 12
+                    queue.insert(0, [current_task, rest])
+
+            if queue and queue[-1][1] == time_required:
+                val = queue.pop()[0] # 0
+                heapq.heappush(max_heap, val)
+            
+            
+        return time_required
+        
+        
+            
+        
+        
