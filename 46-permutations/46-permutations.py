3c45d447f3167cde7c699397c2407909ab0f7ca9
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-13 17:42:29 +0100

    Time: 38 ms (57.93%), Space: 13.5 MB (86.49%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -2,7 +2,7 @@ class Solution(object):
     def permute(self, nums):
         def dfs(nums, permutation):
             if len(nums) == 0:
-                result.append(permutation)
+                result.append(permutation[:])
             for i in range(len(nums)):
                 dfs(nums[:i]+nums[i+1:], permutation+[nums[i]])
         
