b4eca403ed9260969ebedb9931ae797a0179e049
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-15 12:05:49 +0100

    Time: 484 ms (38.62%), Space: 32.5 MB (21.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,7 +1,52 @@
 class Solution(object):
     def numIslands(self, grid):
         def dfs(row, col):
-            if (row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == "0" or (row, col) in visited):
+            stack = [(row, col)]
+            
+            while stack:
+                current_row, current_col = stack.pop()
+                if not (current_row < 0 or current_row >= rows or
+                    current_col < 0 or current_col >= cols or
+                    grid[current_row][current_col] == "0"  or
+                    (current_row, current_col) in visited):
+                    
+                    visited.add((current_row, current_col))
+                    
+                    
+                    stack.append((current_row-1, current_col))
+                    stack.append((current_row+1, current_col))
+                    stack.append((current_row,   current_col-1))
+                    stack.append((current_row, current_col+1))
+            
+            
+            """dfs(row-1, col) # top direction
+            dfs(row+1, col) # down direction
+            dfs(row, col+1) # right direction
+            dfs(row, col-1) # left direction"""
+            
+        if not grid: return 0
+        visited = set()
+        islands_counter = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '1' and (row, col) not in visited:
+                    dfs(row, col)
+                    islands_counter += 1
+
+        return islands_counter
+    
+    
+    
+    
+    """def numIslands(self, grid):
+        def dfs(row, col):
+            if (row < 0 or row >= rows or
+                col < 0 or col >= cols or
+                grid[row][col] == "0"  or
+                (row, col) in visited):
                 return
 
             visited.add((row, col))
@@ -22,5 +67,4 @@ class Solution(object):
                     dfs(row, col)
                     islands_counter += 1
 
-        return islands_counter
-                    
+        return islands_counter"""
