35f532a7846c1c6802fab3231bccd4f32dde6cf0
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-15 18:00:39 +0100

    Time: 702 ms (18.57%), Space: 32.3 MB (25.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,27 +2,18 @@ class Solution(object):
     def numIslands(self, grid):
         def dfs(row, col):
             stack = [(row, col)]
-            
             while stack:
                 current_row, current_col = stack.pop()
                 if not (current_row < 0 or current_row >= rows or
-                    current_col < 0 or current_col >= cols or
-                    grid[current_row][current_col] == "0"  or
-                    (current_row, current_col) in visited):
+                        current_col < 0 or current_col >= cols or
+                        grid[current_row][current_col] == "0"  or
+                        (current_row, current_col) in visited):
                     
-                    visited.add((current_row, current_col))
-                    
-                    
-                    stack.append((current_row-1, current_col))
-                    stack.append((current_row+1, current_col))
-                    stack.append((current_row,   current_col-1))
-                    stack.append((current_row, current_col+1))
-            
-            
-            """dfs(row-1, col) # top direction
-            dfs(row+1, col) # down direction
-            dfs(row, col+1) # right direction
-            dfs(row, col-1) # left direction"""
+                    visited.add(( current_row,   current_col))                    
+                    stack.append((current_row-1, current_col))   # top direction
+                    stack.append((current_row+1, current_col))   # down direction
+                    stack.append((current_row,   current_col-1)) # right direction
+                    stack.append((current_row,   current_col+1)) # left direction
             
         if not grid: return 0
         visited = set()
@@ -40,7 +31,7 @@ class Solution(object):
     
     
     
-    
+        # recursive approach
     """def numIslands(self, grid):
         def dfs(row, col):
             if (row < 0 or row >= rows or
