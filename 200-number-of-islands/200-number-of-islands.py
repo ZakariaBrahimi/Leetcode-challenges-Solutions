eefac3aa0a6c54df814b275c26ca3cc33c046c4f
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-15 11:33:18 +0100

    Time: 469 ms (41.92%), Space: 35.4 MB (5.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,18 +1,15 @@
 class Solution(object):
     def numIslands(self, grid):
         def dfs(row, col):
-            if (row < 0 or row >= rows or col < 0 or col >= cols ):
+            if (row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == "0" or (row, col) in visited):
                 return
+
+            visited.add((row, col))
+            dfs(row-1, col) # top direction
+            dfs(row+1, col) # down direction
+            dfs(row, col+1) # right direction
+            dfs(row, col-1) # left direction
             
-            
-            if grid[row][col] == '1':
-                grid[row][col] = 0   
-                dfs(row-1, col)   # top direction
-                dfs(row+1, col)   # down direction
-                dfs(row,   col+1) # right direction
-                dfs(row,   col-1) # left direction
-            return
-                
         if not grid: return 0
         visited = set()
         islands_counter = 0
@@ -21,11 +18,9 @@ class Solution(object):
         
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == '1':
+                if grid[row][col] == '1' and (row, col) not in visited:
                     dfs(row, col)
                     islands_counter += 1
-                    
-        
-        
+
         return islands_counter
                     
