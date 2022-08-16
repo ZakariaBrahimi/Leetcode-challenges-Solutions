7f1d9bbfc3068688802cc5a0458d727b510bd045
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-16 11:17:28 +0100

    Time: 185 ms (46.06%), Space: 16.6 MB (20.81%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -11,12 +11,12 @@ class Solution(object):
                 return
             visited.add((row, col))
             current_area[0] += 1
-            
+            max_area[0]      = max(max_area[0], current_area[0])
             dfs(row+1, col) # down direction 
             dfs(row-1, col) # top direction
             dfs(row, col+1) # right direction
             dfs(row, col-1) # left direction
-            max_area[0]      = max(max_area[0], current_area[0])
+            
             
         
         for row in range(rows):
