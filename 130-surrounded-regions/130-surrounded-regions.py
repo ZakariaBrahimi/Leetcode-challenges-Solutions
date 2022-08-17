9f5c367103ac3e40923ba3cd37634976801a1465
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-17 20:42:32 +0100

    Time: 215 ms (31.58%), Space: 19 MB (69.47%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,53 +1,5 @@
 class Solution(object):
     def solve(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
-        """
-        # Time Complexity : O(n*m)
-        # Space Complexity: O(n*m)
-        if not board or not board[0]: return []
-        ROWS = len(board)
-        COLS = len(board[0])
-        hasVisited = set()
-        unsurrounded_regions = set()
-        
-        def mark(row, col):
-            # base cases: out of bounds & equal to 'X' & not visited
-            if row<0 or row == ROWS or col<0 or col == COLS: return
-            if board[row][col] == 'X': return
-            if (row, col) in hasVisited or (row, col) in unsurrounded_regions: return
-            
-            hasVisited.add((row, col))
-            unsurrounded_regions.add((row, col))
-            mark(row+1, col)
-            mark(row-1, col)
-            mark(row, col+1)
-            mark(row, col-1)
-        
-        # 1. mark all unsurrounded  regions with 1
-        # surrounded regiens are in border or adjacent to it.
-        for row in range(ROWS):
-            for col in range(COLS):
-                if (board[row][col] == 'O' and
-                   (row, col) not in hasVisited and 
-                   (row in [0, ROWS-1] or col in [0, COLS-1])):
-                    mark(row, col)
-        
-        for row in range(ROWS):
-            for col in range(COLS):
-                if (row, col) not in unsurrounded_regions:
-                    board[row][col] = 'X'                
-                    
-        return board
-
-
-'''class Solution(object):
-    def solve(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
-        """
         # Time Complexity : O(n*m)
         # Space Complexity: O(n*m)
         if not board or not board[0]: return []
@@ -57,7 +9,7 @@ class Solution(object):
         
         def mark(row, col):
             # base cases: out of bounds & equal to 'X' & not visited
-            if row<0 or row == ROWS or col<0 or col == COLS: return
+            if row<0 or row == ROWS or col<0 or col == COLS: return # out of bounds
             if board[row][col] == 'X': return
             if (row, col) in hasVisited: return
             
@@ -86,4 +38,44 @@ class Solution(object):
                 else:
                     board[row][col] = 'O'
         
-        return board'''
+        return board
+    
+    
+
+    # Another similar approach
+        """
+        if not board or not board[0]: return []
+        ROWS = len(board)
+        COLS = len(board[0])
+        hasVisited = set()
+        unsurrounded_regions = set()
+        
+        def mark(row, col):
+            # base cases: out of bounds & equal to 'X' & not visited
+            if row<0 or row == ROWS or col<0 or col == COLS: return # Out of bounds
+            if board[row][col] == 'X': return
+            if (row, col) in hasVisited or (row, col) in unsurrounded_regions: return
+            
+            hasVisited.add((row, col))
+            unsurrounded_regions.add((row, col))
+            mark(row+1, col)
+            mark(row-1, col)
+            mark(row, col+1)
+            mark(row, col-1)
+        
+        # 1. mark all unsurrounded  regions with 1
+        # surrounded regiens are in border or adjacent to it.
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (board[row][col] == 'O' and
+                   (row, col) not in hasVisited and 
+                   (row in [0, ROWS-1] or col in [0, COLS-1])):
+                    mark(row, col)
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row, col) not in unsurrounded_regions:
+                    board[row][col] = 'X'                
+                    
+        return board
+        """
