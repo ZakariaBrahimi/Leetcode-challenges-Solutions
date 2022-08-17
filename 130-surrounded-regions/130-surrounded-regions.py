ae514decc6a6c8992a13fe9c3f209a3ff207d2b3
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-17 20:33:46 +0100

    Time: 152 ms (71.58%), Space: 19 MB (86.90%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -4,41 +4,42 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
+        # Time Complexity : O(n*m)
+        # Space Complexity: O(n*m)
         if not board or not board[0]: return []
-        rows = len(board)
-        cols = len(board[0])
+        ROWS = len(board)
+        COLS = len(board[0])
         hasVisited = set()
         
-        def mark(row, col, markType):
-            # base cases: out of bounds | equal to 'X'
-            if row<0 or row == rows or col<0 or col == cols: return
+        def mark(row, col):
+            # base cases: out of bounds & equal to 'X' & not visited
+            if row<0 or row == ROWS or col<0 or col == COLS: return
             if board[row][col] == 'X': return
             if (row, col) in hasVisited: return
             
             hasVisited.add((row, col))
             board[row][col] = 1
-            mark(row+1, col, 1)
-            mark(row-1, col, 1)
-            mark(row, col+1, 1)
-            mark(row, col-1, 1)
+            mark(row+1, col)
+            mark(row-1, col)
+            mark(row, col+1)
+            mark(row, col-1)
         
-        # 1. mark all wrong regions with 1
-        # Wrong regiens are in border or adjacent to it
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] == 'O' and (row, col) not in hasVisited and (row in [0, rows-1] or col in [0, cols-1]):
-                    mark(row, col, 1)
+        # 1. mark all unsurrounded  regions with 1
+        # surrounded regiens are in border or adjacent to it.
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (board[row][col] == 'O' and
+                   (row, col) not in hasVisited and 
+                   (row in [0, ROWS-1] or col in [0, COLS-1])):
+                    mark(row, col)
         
-        # 2. mark all remaining regiens with 'X'
-        for row in range(rows):
-            for col in range(cols):
+        # 2. mark all remaining 'O' regions with 'X'
+        for row in range(ROWS):
+            for col in range(COLS):
                 if board[row][col] != 1:
                     board[row][col] = 'X'
-        
-        # 3. mark all '1' to 'O'
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] == 1:
+        # 3. mark all '1' regions with 'O'
+                else:
                     board[row][col] = 'O'
         
         return board
