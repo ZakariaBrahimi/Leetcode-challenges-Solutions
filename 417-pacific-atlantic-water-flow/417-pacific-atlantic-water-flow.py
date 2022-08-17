a2eb72bec1d61da797b97120427ced3653385656
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-17 09:36:12 +0100

    Time: 548 ms (27.98%), Space: 15.8 MB (22.05%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -0,0 +1,36 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]: return []
+        rows     = len(heights)
+        cols     = len(heights[0])
+        pacific  = []
+        atlantic = []
+        pacific_visited  = set()
+        atlantic_visited = set()
+        directions = [(0,1), (0,-1), (1,0), (-1,0)] # 4 directions
+        
+        # Starting
+        for col in range(cols):
+            pacific.append((0, col))
+            atlantic.append((rows-1, col))
+        for row in range(rows):
+            pacific.append((row, 0))
+            atlantic.append((row, cols-1))
+        
+        
+        def dfs(row, col, visitedOcean):
+            visitedOcean.add((row, col))
+            for rowDirection, colDirection in directions:
+                new_row = rowDirection + row
+                new_col = colDirection + col
+                if ( new_row >= 0  and new_row < rows and  new_col >= 0 and new_col < cols and 
+                   ( new_row, new_col) not in visitedOcean and heights[new_row][new_col] >= heights[row][col]):
+                    dfs(new_row, new_col, visitedOcean)
+                
+        
+        
+        for row, col in pacific:  dfs(row, col, pacific_visited)
+        for row, col in atlantic: dfs(row, col, atlantic_visited)
+        
+        return pacific_visited & atlantic_visited # Intersection between 2 sets, (and ==> &&)
+        
