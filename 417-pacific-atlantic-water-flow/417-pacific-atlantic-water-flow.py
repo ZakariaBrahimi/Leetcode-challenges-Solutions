20adecb6561777d21906a7ffe8d77ca971728259
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-17 10:48:41 +0100

    Time: 478 ms (27.04%), Space: 17 MB (7.50%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,5 +1,7 @@
 class Solution:
     def pacificAtlantic(self, heights):
+        # Time Complexity : 3m + 3n + 4n*m = O(n*m)
+        # Space Complexity: 2n + 2m + n*m  = O(n*m) 
         if not heights or not heights[0]: return []
         rows     = len(heights)
         cols     = len(heights[0])
@@ -17,23 +19,23 @@ class Solution:
             pacific.append((row, 0))
             atlantic.append((row, cols-1))
         
-        
         def dfs(row, col, visitedOcean):
-            # base case: visted
-            if (row, col) in visitedOcean:
-                return
+            # base case: visted,but it's doesn't work,
+            # cuz we have alreary check if visited or not befor go with recursive call
+            """if (row, col) in visitedOcean:
+                return"""
             visitedOcean.add((row, col))
             for rowDirection, colDirection in directions:
                 new_row = rowDirection + row
                 new_col = colDirection + col
-                if ( new_row >= 0  and new_row < rows and  new_col >= 0 and new_col < cols and 
-                   ( new_row, new_col) not in visitedOcean and heights[new_row][new_col] >= heights[row][col]):
+                if ( rows > new_row >= 0 and  
+                     cols > new_col >= 0 and 
+                   ( new_row, new_col) not in visitedOcean and
+                     heights[new_row][new_col] >= heights[row][col]):
                     dfs(new_row, new_col, visitedOcean)
                 
-        
-        
         for row, col in pacific:  dfs(row, col, pacific_visited)
         for row, col in atlantic: dfs(row, col, atlantic_visited)
         
-        return pacific_visited & atlantic_visited # Intersection between 2 sets, (and ==> &&)
+        return pacific_visited & atlantic_visited # Intersection between 2 sets, (and ==> && not one of &)
         
