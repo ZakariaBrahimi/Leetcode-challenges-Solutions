b2f5c3468e1ecefecf50a4b87595598b7a3821f5
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-20 19:04:47 +0100

    Time: 75 ms (93.87%), Space: 16.7 MB (18.31%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,38 +1,31 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        # [[0,1], [0,2], [1,3], [1,4], [3,4]] , numCourses = 5
-        stack = []
-        hasVisited = set()
-        hash_map = {i:[] for i in range(numCourses)} # {0:[], 1:[], 2:[], 3:[], 4:[]}
+        # Test Case:
+        #   [[0,1], [0,2], [1,3], [1,4], [3,4]] , numCourses = 5
+        #   hash_map = {0:[1,2], 1:[3,4], 2:[], 3:[4], 4:[]}
+        #   hasVisited = {0, 1, 3, 4}
         
+        hasVisited = set()
+        hash_map   = {i:[] for i in range(numCourses)} # {0:[], 1:[], 2:[], 3:[], 4:[]}
+        # Mapping each course to : prereq list
         for course in prerequisites: # prerequisite = 
             hash_map[course[0]].append(course[1])
-        # hash_map = {0:[1,2], 1:[3,4], 2:[], 3:[4], 4:[]}
         
-        
-        
-        # hasVisited = {0, 1, 3, 4}
-        # stack = [4,3]
-        def dfs(key):
-            if key in hasVisited: return False
-            if hash_map[key] == []:
-                # hasVisited.add(key)
+        def dfs(course):
+            if course in hasVisited: return False
+            if hash_map[course] == []:
                 return True
             
-            hasVisited.add(key)
-            for neighbor in hash_map[key]:
+            hasVisited.add(course)
+            for neighbor in hash_map[course]:
                 if not dfs(neighbor): # not dfs(neighbor) == dfs(neighbor) == False
                     return False
-            hasVisited.remove(key)
-            hash_map[key] = []
-            #stack.append(key)
+            hasVisited.remove(course)
+            hash_map[course] = []
             return True
-            
-        for key in hash_map:
-            # if key not in hasVisited:
-                # result = dfs(key)
-                
-            if not dfs(key):
+        # We should run this loop in case the graph is not fully connected
+        for course in hash_map:
+            if not dfs(course):
                 return False
             
         return True
