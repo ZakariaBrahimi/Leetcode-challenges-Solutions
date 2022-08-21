a4875be3c3d78e5e1d6d2cbe0b7b9dab0d9c7760
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-21 18:52:34 +0100

    Time: 31 ms (26.15%), Space: 13.7 MB (12.61%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        steps = []
+        memo = {}
+        
+        def memoization(n):
+            if n in memo: return memo[n]
+            if n == 0: return 1
+            if n < 0 : return 0
+            
+            memo[n] = memoization(n-1) + memoization(n-2)
+            return memo[n]
+        
+        memoization(n)
+        return memo[n]
