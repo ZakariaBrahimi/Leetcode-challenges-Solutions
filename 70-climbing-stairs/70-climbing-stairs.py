039c5417c2d5d1a23d8e93e5c8bc46eaacdf6cf9
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-22 11:03:39 +0100

    Time: 21 ms (71.09%), Space: 13.4 MB (63.62%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,5 +1,14 @@
 class Solution(object):
     def climbStairs(self, n):
+        one = 1
+        two = 1
+        for i in range(2, n+1):
+            temp = one
+            one = one+two
+            two = temp
+        return one
+        
+        '''
         dp = [0]*(n+1)
         dp[0] = 1
         dp[1] = 1
@@ -7,7 +16,7 @@ class Solution(object):
             dp[i] = dp[i-1] + dp[i-2]
         
         return dp[-1]
-        
+        '''
         
         '''
         steps = []
