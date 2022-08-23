4470c89df9c9a867850132db5822478af99dd67c
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-23 10:49:37 +0100

    Time: 85 ms (16.07%), Space: 16.1 MB (6.23%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,10 +1,24 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
-        cost.append(0)
+        def solve(steps):
+            if steps in memo: return memo[steps]
+            if steps == 0: return cost[0]
+            if steps == 1: return cost[1]
+            
+            memo[steps] = cost[steps] + min(solve(steps-1), solve(steps-2))
+            return memo[steps]
+        memo = dict()
+        steps = len(cost)
+        return min(solve(steps-1), solve(steps-2))
+        
+        
+        # Using bottom-up (tabulation) approach
+        """
+        cost.append(0) # the cost of setting on the top of the floor is 0
         
         for i in range(len(cost)-3, -1, -1):
             cost[i] = cost[i] + min(cost[i+1], cost[i+2])
         
         # We can either start from the step with index 0, or the step with index 1.
-        return min(cost[0], cost[1])
+        return min(cost[0], cost[1])"""
             
