8989b0d10a25cae75f8792a317ea8f7564ba1f11
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-25 10:22:53 +0100

    Time: 287 ms (9.15%), Space: 17 MB (46.77%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        most_frequent_elements = []
+        hashMap = Counter(nums)
+        current_val = -1
+        while k!=0:
+            for key, value in hashMap.items():
+                if value > current_val:
+                    current_val = value
+                    current_key = key
+            most_frequent_elements.append(current_key)
+            hashMap[current_key] = -1
+            k -= 1
+            current_val = -1
+            current_key = -1
+        
+        return most_frequent_elements
+            
+            
+        
