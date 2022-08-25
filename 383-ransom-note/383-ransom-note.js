3abd46929418e4b535d2811493e99fd260c82244
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-25 09:48:11 +0100

    Time: 149 ms (33.58%), Space: 44.9 MB (58.73%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -1,27 +1,3 @@
-/**
- * @param {string} ransomNote
- * @param {string} magazine
- * @return {boolean}
- */
-var canConstruct = function(ransomNote, magazine) {
-    const leters_counter = {}
-    for(let i=0; i<magazine.length; i++){
-        let letter = magazine[i]
-        if (letter in leters_counter){
-            leters_counter[letter] += 1
-        }else{
-            leters_counter[letter] = 0
-        }
-    }
-    
-    for (let letter of ransomNote){
-        if (!leters_counter[letter]){
-            return false
-        }
-        leters_counter[letter]--
-    }
-    return true
-};
 
 var canConstruct = function(ransomNote, magazine) {
     const map = {};
