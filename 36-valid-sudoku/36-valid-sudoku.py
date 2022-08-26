1869955c0a995f1c2bf172ce7cb21f2065ecf052
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-26 12:35:53 +0100

    Time: 175 ms (31.36%), Space: 14 MB (35.29%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,52 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Declaring variables
+        ROWS = len(board)
+        COLS = len(board[0])
+        valid = set()
+        
+        # 1. Check the validation of all rows -> by fixing the row number and move the col number
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == '.':
+                    continue
+                elif board[row][col] in valid:
+                    return False
+                else:
+                    valid.add(board[row][col])
+            valid = set()
+            
+        # 2. Check the validation of all collumns -> by fixing the col number and move the row number
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[col][row] == '.':
+                    continue
+                elif board[col][row] in valid:
+                    return False
+                else:
+                    valid.add(board[col][row])
+            valid = set()
+        
+        # 3. Check the validation of all sub-boxes
+        #      a. Creating the 9 sub-boxes map 
+        #      b. iterate through the board and assing True or False in each sub-boxes
+        #           - if one of them False, return False immediately
+        subBoxes = {}
+        for i in range(3):
+            for j in range(3):
+                subBoxes[(i,j)] = set()
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == '.':
+                    continue
+                else:
+                    row_area, col_area = (row//3, col//3)
+                    if board[row][col] in subBoxes[(row_area,col_area)]:
+                        print(subBoxes)
+                        return False
+                subBoxes[(row_area,col_area)].add(board[row][col])
+        
+        # Note: if one of the last 3 steps given a False -> immediately return False
+        
+        # 4. If the last 3 steps are passed without returning False -> return True
+        return True
