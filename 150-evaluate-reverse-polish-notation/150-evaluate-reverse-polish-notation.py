078f9a0ff58f69cfac987ecb56846f0e18431ebc
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-27 18:54:05 +0100

    Time: 123 ms (32.30%), Space: 14.4 MB (59.81%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,37 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # 0. Declaring variables
+        stack = []
+        operations = {'+', '-', '*', '/'}
+        
+        # 1. Iterate through out the given array
+        for item in tokens:
+        # 2. Keep pushing to the stack until we reach one of operators
+            if item not in operations:
+                stack.append(int(item))
+            else:
+        # 3. Popinh the last 2 pushed numbers and do the operation
+                if item == '+':
+                    left    = stack.pop()
+                    right   = stack.pop()
+                    newItem = left + right
+        # 4. Re-pushing the result again
+                    stack.append(newItem)
+                if item == '-':
+                    right   = stack.pop()
+                    left    = stack.pop()
+                    newItem = left - right
+                    stack.append(newItem)
+                if item == '*':
+                    left    = stack.pop()
+                    right   = stack.pop()
+                    newItem = left * right
+                    stack.append(newItem)
+                if item == '/':
+                    right   = stack.pop()
+                    left    = stack.pop()
+                    newItem = left / right
+                    stack.append(int(newItem))
+        
+        # 5. If we reach the end of the given list push the last remaining elemnt from the stack and return it.
+        return stack.pop()
