5d4317e6c12cd651dbf86efc5eb01a48e8320a01
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-27 08:30:42 +0100

    Time: 402 ms (12.02%), Space: 14 MB (58.01%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,22 +1,25 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # Time Complexity : O(n), n= length of the given string
+        # Space Complexity: O(n)
+        # Valid Sub-string: (substring.length) - (number of leteres that repeated the most) <= k
         charCounter = {}
         left, right = 0, 0
         window = 0
-        result = 0
+        result = 0 
         
-        # Valid Sub-string: (substring.length) - (number of leteres that repeated the most) <= k 
-        
-        # 1. iterate through out the string until right pointer reach the end of the string
+        # 1. iterate through out the string until the right pointer reach the end of the string
         while right < len(s):
-        # 3. Expand the window by one then shift the right pointer, until we reach the valid substring
+        # 2. Expand the window by one then shift the right pointer, until we reach the valid substring
             window += 1
-            charCounter[s[right]] = 1 + charCounter.get(s[right], 0)
-            #isValid = len(s[left:right+1]) - max(charCounter.values()) <= k
+            charCounter[s[right]] = 1 + charCounter.get(s[right], 0) 
             right  += 1
-            if (len(s[left:right]) - max(charCounter.values())) <= k:
+            # if the sub-string is valid
+            if (len(s[left:right]) - max(charCounter.values())) <= k: 
                 result = max(result, window)
+            # if the sub-string is not valid
             else:
+                # Keep shift the left pointer until we find the valid sub-string and than shift the right pointer
                 while not ((len(s[left:right]) - max(charCounter.values())) <= k):
                     window -= 1
                     charCounter[s[left]] -= 1
