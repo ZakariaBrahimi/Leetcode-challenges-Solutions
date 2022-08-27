048e99deced27b4eed91200d556f869973509154
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-08-27 11:51:31 +0100

    Time: 2279 ms (19.06%), Space: 13.8 MB (99.79%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,13 +1,5 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        # 1. Initializing 2 pointer with 0 & char counter for s1
-        
-        # 2. Iterating through out the string until the right poniter reach the end of the given string
-        
-        # 3. Expanding the window until we find all chars on the created counter
-        
-        # 4. Shrinking the window until we reach the length of s1
-        
+    def checkInclusion(self, s1: str, s2: str) -> bool:        
         left, right = 0, len(s1) - 1
         charCounter = Counter(s1)
         
