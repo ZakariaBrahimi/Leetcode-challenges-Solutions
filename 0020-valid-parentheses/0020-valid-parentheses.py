a38b57054fab2e22274a760a32d6ac38d9237dc2
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2024-10-09 11:53:29 +0100

    Time: 31 ms (81%), Space: 16.6 MB (15.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,12 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # stack = []
+        # for parenthes in s:
+        #     if stack and ((stack[-1] + parenthes == '()') or 
+        #        (stack[-1] + parenthes == '[]') or
+        #        (stack[-1] + parenthes == "{}")) :
+        #        stack.pop()
+        #     else:
+        #         stack.append(parenthes)
+
+        # return True if not stack else False
+        
         stack = []
+        parenthesMap = {
+            '(' : ')',
+            '[' : ']',
+            '{' : '}',
+        }
         for parenthes in s:
-            if stack and ((stack[-1] + parenthes == '()') or 
-               (stack[-1] + parenthes == '[]') or
-               (stack[-1] + parenthes == "{}")) :
-               stack.pop()
-            else:
-                stack.append(parenthes)
+            # if it is closing parenthes
+            if parenthes not in parenthesMap:
+                if stack and stack[-1] == parenthes:
+                    stack.pop()
+                else:
+                    return False
+
+            # if it is openning parenthes
+            if parenthes in parenthesMap:
+                stack.append(parenthesMap[parenthes])
 
         return True if not stack else False
