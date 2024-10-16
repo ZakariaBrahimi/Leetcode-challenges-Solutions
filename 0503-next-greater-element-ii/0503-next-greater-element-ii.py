fb29a7d109e65d0857193cf5276379d753d4f40e
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2024-10-16 08:53:38 +0100

    Time: 147 ms (75.68%), Space: 18.9 MB (7.55%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,9 +1,7 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        # if nums[i] > stack[-1] 
-            # stack.pop[-1]
-            # stack.append(nums[i])
-            # result[k] = nums[i]
+        # Time  Complexity: O(n)
+        # Space Complexity: O(n)
 
             stack = []
             result = [-1] * len(nums)
@@ -14,9 +12,7 @@ class Solution:
                     top_val, index = stack.pop()
                     result[index] = current
 
-                # if not stack or current < stack[-1][0]:
                 stack.append([current, i])
-
 
             for i in range(len(nums)):
                 current = nums[i]
@@ -24,12 +20,4 @@ class Solution:
                     top_val, index = stack.pop()
                     result[index] = current
                 
-                
-            # nums = [1,2,3,4,3]
-            # current = 3
-            # stack = [(4,3), (3, 4)]
-            # result = [2, 3, 4, -1, 4]
-
-            
-
             return result
